2873f868d231c6137c4dc3ad26838248088f9c09
Jakub Nakielski <jakubnakielski2@gmail.com>
2019-11-12 20:40:29 +0100

    Add favicon

--- build/js/script.js
+++ build/js/script.js
@@ -1,2 +1,8 @@
 const btn = document.getElementById("heroBtn");
-btn.addEventListener("click", () => {});
+const hero = document.querySelector(".mainHero");
+
+btn.addEventListener("click", e => {
+  const heroHeight = hero.getBoundingClientRect().height;
+  console.log(e);
+  window.scrollTo(0, heroHeight);
+});
