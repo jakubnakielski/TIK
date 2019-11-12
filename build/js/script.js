9573ce05c89020fac07a9a8efb5d0d70d2e7463c
Jakub Nakielski <jakubnakielski2@gmail.com>
2019-11-12 21:49:30 +0100

    Add article header

--- build/js/script.js
+++ build/js/script.js
@@ -3,6 +3,16 @@ const hero = document.querySelector(".mainHero");
 
 btn.addEventListener("click", e => {
   const heroHeight = hero.getBoundingClientRect().height;
-  console.log(e);
+  //   smoothScroll(heroHeight);
   window.scrollTo(0, heroHeight);
 });
+
+// let currentHeight = 0;
+// function smoothScroll(height) {
+//   currentHeight++;
+//   console.log(currentHeight);
+//   if (currentHeight <= parseInt(height)) {
+//     window.scrollTo(0, currentHeight);
+//     var frame = requestAnimationFrame(smoothScroll);
+//   } else cancelAnimationFrame(frame);
+// }
